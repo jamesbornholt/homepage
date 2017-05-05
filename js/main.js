6aca3e7a8acbd3969cd697af2354e85ee44c5de2
James Bornholt <bornholt@cs.washington.edu>
2017-05-04 20:53:15 -0700

    compress stuff; remove dependencies

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
-$(function() {
-    // fix up the mailto links
-    $("#contact span").remove();
-    $("#contact").attr("href", "mailto:" + $.trim($("#contact").text()));
+function enableMailto() {
+    var contact = document.getElementById("contact");
+    var spans = contact.querySelectorAll("span");
+    console.log(spans);
+    for (var i = 0; i < spans.length; i++) {
+        spans[i].parentNode.removeChild(spans[i]);
+    }
+    var txt = contact.innerText;
+    contact.setAttribute("href", "mailto:" + txt.trim());
+}
 
+function highlightRosette() {
     // hack to highlight rosette code
     var keywords = ["define-symbolic", "solve", "assert"];
-    for (var i = 0; i < keywords.length; i++)
-        $(".n:contains('" + keywords[i] + "')").removeClass("n").addClass("k");
-});
+    var elts = document.querySelectorAll(".n");
+    for (var i = 0; i < elts.length; i++) {
+        var txt = elts[i].innerHTML;
+        for (var j = 0; j < keywords.length; j++) {
+            if (txt.indexOf(keywords[j]) > -1) {
+                elts[i].classList.remove("n");
+                elts[i].classList.add("k");
+                break;
+            }
+        }
+    }
+}
+
+function init() {
+    enableMailto();
+    highlightRosette();
+}
+
+document.addEventListener("DOMContentLoaded", init);
